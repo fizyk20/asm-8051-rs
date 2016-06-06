ecd1aed680630cc8d13c7b02846d5014e208d434
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-06-06 13:30:41 +0200

    Added Tokenizer::tokenize and tests

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,12 +1,14 @@
 use std::mem;
+use std::fmt::Debug;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct Position {
     row: usize,
     column: usize
 }
 
 /// An enumeration listing possible tokens
+#[derive(Debug)]
 pub enum Token {
     Identifier(String, Position),
     Number(String, Position),
@@ -19,6 +21,7 @@ pub enum Token {
 }
 
 /// Possible tokenizer states
+#[derive(Debug)]
 enum TokenizerState {
     Ready,
     ReadingNumber(Vec<char>, Position),
@@ -36,6 +39,7 @@ pub struct Tokenizer {
     state: TokenizerState
 }
 
+#[derive(Debug)]
 pub enum TokenizerError {
     GeneralError,
     UnexpectedCharacter(Position, char)
@@ -43,7 +47,7 @@ pub enum TokenizerError {
 
 impl Tokenizer {
 
-    pub fn new(s: String) -> Tokenizer {
+    pub fn new(s: &str) -> Tokenizer {
         Tokenizer {
             tokens: Vec::new(),
             string: s.chars().collect(),
@@ -57,13 +61,12 @@ impl Tokenizer {
         self.string[self.position]
     }
 
-    fn next_char(&self) -> Option<char> {
-        if self.position + 1 < self.string.len() {
-            Some(self.string[self.position + 1])
-        }
-        else {
-            None
-        }
+    fn eof(&self) -> bool {
+        self.position >= self.string.len()
+    }
+
+    fn get_tokens(self) -> Vec<Token> {
+        self.tokens
     }
 
     fn advance(&mut self) {
@@ -79,11 +82,13 @@ impl Tokenizer {
     fn handle_ready(&mut self, c: char) -> Option<TokenizerError> {
         match c {
             ' ' | '\t' | '\r' => {
+                self.state = TokenizerState::Ready;
                 self.advance();
                 None
             },
 
             '\n' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::Newline(self.curPos));
                 self.advance();
                 self.newline();
@@ -103,30 +108,35 @@ impl Tokenizer {
             },
 
             ':' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::Colon(self.curPos));
                 self.advance();
                 None
             },
 
             ',' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::Comma(self.curPos));
                 self.advance();
                 None
             },
 
             '@' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::At(self.curPos));
                 self.advance();
                 None
             },
 
             '#' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::Hash(self.curPos));
                 self.advance();
                 None
             },
 
             '+' => {
+                self.state = TokenizerState::Ready;
                 self.tokens.push(Token::Plus(self.curPos));
                 self.advance();
                 None
@@ -138,7 +148,10 @@ impl Tokenizer {
                 None
             },
 
-            _ => Some(TokenizerError::UnexpectedCharacter(self.curPos, c))
+            _ => {
+                self.state = TokenizerState::Ready;
+                Some(TokenizerError::UnexpectedCharacter(self.curPos, c))
+            }
         }
     }
 
@@ -152,6 +165,7 @@ impl Tokenizer {
             },
 
             _ => {
+                self.state = TokenizerState::ReadingComment;
                 self.advance();
                 None
             }
@@ -192,7 +206,6 @@ impl Tokenizer {
             _ => {
                 self.tokens.push(Token::Identifier(v.into_iter().collect(), p));
                 self.state = TokenizerState::Ready;
-                self.advance();
                 None
             }
         }
@@ -218,6 +231,106 @@ impl Tokenizer {
             },
 
             TokenizerState::Invalid => panic!("Tokenizer caught in invalid state")
+        }
+    }
+
+    pub fn tokenize(text: &str) -> Result<Vec<Token>, TokenizerError> {
+        let mut tokenizer = Tokenizer::new(text);
+        loop {
+            if tokenizer.eof() {
+                return Ok(tokenizer.get_tokens());
+            }
+            if let Some(err) = tokenizer.consume_char() {
+                return Err(err);
+            }
+        }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tokenize() {
+        let text = "label: operator operand1, 0EFh ; comment";
+        if let Ok(result) = Tokenizer::tokenize(text) {
+            assert_eq!(result.len(), 6);
+
+            // label
+            if let Token::Identifier(ref s, pos) = result[0] {
+                assert_eq!(s, "label");
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 1);
+            }
+            else {
+                panic!("result[0] is not an Identifier!");
+            }
+
+            // :
+            if let Token::Colon(pos) = result[1] {
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 6);
+            }
+            else {
+                panic!("result[1] is not a Colon!");
+            }
+
+            // operator
+            if let Token::Identifier(ref s, pos) = result[2] {
+                assert_eq!(s, "operator");
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 8);
+            }
+            else {
+                panic!("result[2] is not an Identifier!");
+            }
+
+            // operand1
+            if let Token::Identifier(ref s, pos) = result[3] {
+                assert_eq!(s, "operand1");
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 17);
+            }
+            else {
+                panic!("result[3] is not an Identifier!");
+            }
+
+            // ,
+            if let Token::Comma(pos) = result[4] {
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 25);
+            }
+            else {
+                panic!("result[4] is not a Comma!");
+            }
+
+            // 0EFh
+            if let Token::Number(ref s, pos) = result[5] {
+                assert_eq!(s, "0EFh");
+                assert_eq!(pos.row, 1);
+                assert_eq!(pos.column, 27);
+            }
+            else {
+                panic!("result[5] is not a Number!");
+            }
+        }
+        else {
+            panic!("Tokenization failed!");
+        }
+    }
+
+    #[test]
+    fn test_invalid_number() {
+        let text = "0abcdefgh";
+        if let Err(TokenizerError::UnexpectedCharacter(pos, c)) = Tokenizer::tokenize(text) {
+            assert_eq!(pos.row, 1);
+            assert_eq!(pos.column, 8);
+            assert_eq!(c, 'g');
+        }
+        else {
+            panic!("Tokenization successful!");
         }
     }
 
